dd8941110f7cadc4d8f0a18180a82d2ca33eff27
veteroner <veteroner@gmail.com>
2025-06-22 10:08:58 +0300

    Netlify otomatik deployment setup

--- android/app/src/main/java/com/bilgoo/quizapp/MainActivity.java
+++ android/app/src/main/java/com/bilgoo/quizapp/MainActivity.java
@@ -1,5 +1,19 @@
 package com.bilgoo.quizapp;
 
+import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Android 12+ Splash Screen API
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        
+        // Splash screen'i özelleştir
+        splashScreen.setKeepOnScreenCondition(() -> false);
+        
+        super.onCreate(savedInstanceState);
+    }
+}
